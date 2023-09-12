b1f8b467f5fdc48ec412f054004b73124b1b7145
Fuad Maulana <fuadm03@gmail.com>
2023-09-12 16:56:17 +0700

    feat: get book data when submit

--- main.js
+++ main.js
@@ -6,5 +6,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 const submitHandler = (e) => {
   e.preventDefault();
-  console.log('submitted');
+  console.log(getFormBook());
+}
+
+const getFormBook = () => {
+  return {
+    id: +new Date(),
+    title: document.getElementById('inputJudul').value,
+    author: document.getElementById('inputPenulis').value,
+    year: document.getElementById('inputTahun').value,
+    isComplete: document.getElementById('inputSelesai').checked
+  }
 }
